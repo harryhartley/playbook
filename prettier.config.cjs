2c9597b55a8d17e3ff7c95a21a991595d87b164b
harryhartley <hhartley94@gmail.com>
2023-02-17 16:16:16 +0000

    prettier rules

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,11 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: false,
+  trailingComma: "es5",
+  tabWidth: 2,
 };
