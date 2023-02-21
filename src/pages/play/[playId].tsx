66b0d7b432d3ca70ae10a3cc072ea2ea49d077e8
harryhartley <hhartley94@gmail.com>
2023-02-21 18:11:23 +0000

    fix play import

--- src/pages/play/[playId].tsx
+++ src/pages/play/[playId].tsx
@@ -2,23 +2,12 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { PlayCard } from "../../components/PlayCard";
+import { api } from "../../utils/api";
 
 const Home: NextPage = () => {
   const { query } = useRouter();
   if (typeof query.playId !== 'string') return <p>Bad ID</p>
-  // const play = api.play.getById.useQuery(query.playId);
-  const play = { data: {
-    id: 'a', 
-    name: 'Test Play', 
-    youtubeId: 'Sz6t18jN5M0',
-    description: 'An advanced setup for doombox, utilizing instant flight out of bunt and airturn back spike.',
-    type: "Offense",
-    speed: "All",
-    environment: "Training",
-    character: "Doombox",
-    stage: "All",
-    difficulty: 4,
-  }}
+  const play = api.play.getById.useQuery(query.playId);
   if (!play) return <p>Loading play...</p>
   
   return (
