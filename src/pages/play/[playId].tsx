fcbd01d28138dedd1667982a78bb0211deac825d
harryhartley <hhartley94@gmail.com>
2023-02-24 14:52:58 +0000

    discord embed test 4 - move meta to page

--- src/pages/play/[playId].tsx
+++ src/pages/play/[playId].tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from 'next'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { Play } from '../../components/Play'
 import { api } from '../../utils/api'
@@ -12,6 +13,16 @@ const Home: NextPage = () => {
 
   return (
     <>
+      <Head>
+        <title>LLB Playbook</title>
+        <meta content='Advanced Doombox Spike Setup by CookingTiger' property='og:title' />
+        <meta content='An advanced spike setup with Doombox, utilising air turns' property='og:description' />
+        <meta content='https://playbook-sand.vercel.app/play/cleihuzk30005cxcizs743oim' property='og:url' />
+        <meta content='https://img.youtube.com/vi/lalCEHmS74Y/hqdefault.jpg' property='og:image' />
+        <meta content='#87CEEB' data-react-helmet='true' name='theme-color' />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+
       <main>
         <Play play={play} youtubeEmbed='above' />
       </main>
