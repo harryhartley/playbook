60a5d35e6a3be9e34f807afda4e7208772d041eb
harryhartley <hhartley94@gmail.com>
2023-02-24 12:59:24 +0000

    red unapprove buttonm

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -87,7 +87,7 @@ export const Play = ({ play, youtubeEmbed }: PlayProps) => {
                       unapprovePlay.mutate(id)
                       setHidden('hidden')
                     }}
-                    className='group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-teal-300 to-lime-300 p-0.5 text-sm font-medium text-gray-900 focus:outline-none focus:ring-4 focus:ring-lime-200 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 dark:focus:ring-lime-800'
+                    className='group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-pink-500 to-orange-400 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-pink-200 group-hover:from-pink-500 group-hover:to-orange-400 dark:text-white dark:focus:ring-pink-800'
                   >
                     <span className='relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900'>
                       Unapprove
