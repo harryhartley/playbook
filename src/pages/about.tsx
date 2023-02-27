2bc34a321d3db47c0fde067c836ac8ac2be0e614
harryhartley <hhartley94@gmail.com>
2023-02-27 13:33:10 +0000

    dictionary and about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <div>
+        The Playbook has been designed to assist with the sharing of tech and ideas, outside of Discord servers where
+        posts are easily lost and hard to search.
+      </div>
+    </main>
+  )
+}
+
+export default Home
---
+++ src/pages/dictionary.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <div>Dictionary content here!</div>
+    </main>
+  )
+}
+
+export default Home
