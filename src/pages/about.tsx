5d6ec41c4ab77321f5d27cea065903c9aaeacfea
harryhartley <hhartley94@gmail.com>
2023-03-02 09:18:46 +0000

    add more info to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,20 +3,30 @@ import { type NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <main>
+      <div className='my-2 text-2xl font-bold'>What is the Playbook?</div>
       <div>
         The Playbook has been designed to assist with the sharing of tech and ideas, outside of Discord servers where
-        posts are easily lost and hard to search.
+        posts are easily lost and hard to search. The playbook also offers additional features, for example the ability
+        to filter plays by specific criteria, including character, environment, and even contributor.
       </div>
-      <div className='text-2xl font-bold'>Roadmap</div>
-      <ul>
-        <li>Playbook filtering UI</li>
-        <li>Favourite plays feature</li>
+      <div className='my-2 text-2xl font-bold'>How to contribute Plays</div>
+      <ol className='list-decimal'>
+        <li className='ml-8'>Message hyhy about becoming a contributor</li>
+        <li className='ml-8'>Upload your content to YouTube (or clip an existing video)</li>
+        <li className='ml-8'>Navigate to the submit page</li>
+        <li className='ml-8'>Upload your play with a relevant name, description, and tags</li>
+        <li className='ml-8'>Wait for a moderator to approve the submission!</li>
+      </ol>
+      <div className='my-2 text-2xl font-bold'>Roadmap</div>
+      <ul className='list-disc'>
+        <li className='ml-8'>Playbook filtering UI</li>
+        <li className='ml-8'>Favourite plays</li>
       </ul>
-      <div className='text-2xl font-bold'>Contributors</div>
-      <ul>
-        <li>CookingTiger - Initial content to test UI</li>
-        <li>JawDrop - Initial content to test UI</li>
-        <li>communities - Testing and bugfixes</li>
+      <div className='my-2 text-2xl font-bold'>Contributors</div>
+      <ul className='list-disc'>
+        <li className='ml-8'>CookingTiger - Initial content to test UI</li>
+        <li className='ml-8'>JawDrop - Initial content to test UI</li>
+        <li className='ml-8'>communities - Testing and bugfixes</li>
       </ul>
     </main>
   )
