560597160700a2b25f6600fee80dbbb32d305d79
harryhartley <hhartley94@gmail.com>
2023-03-03 14:22:04 +0000

    seo test

---
+++ src/pages/seotest.tsx
@@ -0,0 +1,20 @@
+import { type NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <meta content={'SEO Test'} property='og:title' />
+        <meta content={'SEO Test Description'} property='og:description' />
+        <meta content={`https://www.youtube.com/watch?v=COCLDCossos`} property='og:url' />
+        <meta content={`https://img.youtube.com/vi/COCLDCossos/hqdefault.jpg`} property='og:image' />
+        <meta content='#87CEEB' data-react-helmet='true' name='theme-color' />
+        {/* <meta name='twitter:card' content='summary_large_image' /> */}
+      </Head>
+      <main>SEO TEST PAGE IGNORE ME</main>
+    </>
+  )
+}
+
+export default Home
