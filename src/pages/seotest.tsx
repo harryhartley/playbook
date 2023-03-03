f8197fb5b70391a9be3c1a857bc4b322318272b1
harryhartley <hhartley94@gmail.com>
2023-03-03 15:09:54 +0000

    seo test 7

--- src/pages/seotest.tsx
+++ src/pages/seotest.tsx
@@ -6,15 +6,19 @@ const Home: NextPage = () => {
     <>
       <Head>
         <meta content={'SEO Test'} property='og:title' />
+        <meta content='Playbook' property='og:site_name' />
         <meta content={'SEO Test Description'} property='og:description' />
         <meta content={'https://www.hyhy.gg/playbook'} property='og:url' />
-        <meta content={'https://www.youtube.com/v/COCLDCossos'} property='og:video' />
-        <meta content={'https://www.youtube.com/v/COCLDCossos'} property='og:video:url' />
-        <meta content={'https://www.youtube.com/v/COCLDCossos'} property='og:video:secure_url' />
-        <meta content={'398'} property='og:video:width' />
-        <meta content={'224'} property='og:video:height' />
-        <meta content={'application/x-shockwave-flash'} property='og:video:type' />
+        <meta content={'video.other'} property='og:type' />
+        <meta content={'https://www.youtube.com/embed/COCLDCossos'} property='og:video' />
+        <meta content={'https://www.youtube.com/embed/COCLDCossos'} property='og:video:url' />
+        <meta content={'https://www.youtube.com/embed/COCLDCossos'} property='og:video:secure_url' />
+        <meta content={'1280'} property='og:video:width' />
+        <meta content={'720'} property='og:video:height' />
+        <meta content={'text/html'} property='og:video:type' />
         <meta content={`https://img.youtube.com/vi/COCLDCossos/hqdefault.jpg`} property='og:image' />
+        <meta property='og:image:width' content='480' />
+        <meta property='og:image:height' content='360' />
         <meta content='#87CEEB' data-react-helmet='true' name='theme-color' />
         {/* <meta name='twitter:card' content='summary_large_image' /> */}
       </Head>
