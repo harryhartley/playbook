f53396fdd596c52e6342673849a52d011352c527
harryhartley <hhartley94@gmail.com>
2023-03-10 14:43:47 +0000

    server side validation on api route

--- src/pages/api/postToWebhook.ts
+++ src/pages/api/postToWebhook.ts
@@ -2,44 +2,59 @@ import type { Play } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { siteMetadata } from '../../data/siteMetadata'
 import { env } from '../../env.mjs'
+import { getServerAuthSession } from '../../server/auth'
 import { getThumbnailUrl } from '../../utils/youtube'
+import { isUserModeratorOrAbove } from './../../utils/auth'
 
-const postToWebhook = (req: NextApiRequest, res: NextApiResponse) => {
+const postToWebhook = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerAuthSession({ req, res })
   const play = JSON.parse(req.body as string) as Play & { user: { name: string | null } }
 
-  const params = {
-    username: siteMetadata.title,
-    avatar_url: '',
-    tts: false,
-    embeds: [
-      {
-        type: 'rich',
-        title: play.name,
-        description: play.description,
-        color: 0x00ffff,
-        timestamp: play.createdAt.toString(),
-        image: {
-          url: getThumbnailUrl(play.youtubeId),
-          height: 0,
-          width: 0,
-        },
-        author: {
-          name: play.user.name,
-          url: `http://localhost:3000/user/${play.userId}`,
-        },
-        url: `http://localhost:3000/play/${play.id}`,
-      },
-    ],
-  }
-  void fetch(env.WEBHOOK_URL, {
-    method: 'POST',
-    body: JSON.stringify(params),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
+  if (!session || !isUserModeratorOrAbove(session?.user.role ?? '')) {
+    res.status(401).end('Not authorised')
+  } else {
+    switch (req.method) {
+      case 'POST':
+        const params = {
+          username: siteMetadata.title,
+          avatar_url: '',
+          tts: false,
+          embeds: [
+            {
+              type: 'rich',
+              title: play.name,
+              description: play.description,
+              color: 0x00ffff,
+              timestamp: play.createdAt.toString(),
+              image: {
+                url: getThumbnailUrl(play.youtubeId),
+                height: 0,
+                width: 0,
+              },
+              author: {
+                name: play.user.name,
+                url: `http://hyhy.gg/user/${play.userId}`,
+              },
+              url: `http://hyhy.gg/play/${play.id}`,
+            },
+          ],
+        }
+        void fetch(env.WEBHOOK_URL, {
+          method: 'POST',
+          body: JSON.stringify(params),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
 
-  res.status(200).json({ success: true, message: `Play ${play.id} successfully posted` })
+        res.status(200).json({ success: true, message: `Play ${play.id} successfully posted` })
+        break
+
+      default:
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${req.method ?? ''} not allowed`)
+    }
+  }
 }
 
 export default postToWebhook
