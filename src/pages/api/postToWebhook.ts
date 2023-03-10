1e91e013773ab89f9a96d71c2a6b90e9f1d0fa28
harryhartley <hhartley94@gmail.com>
2023-03-10 15:41:12 +0000

    webhook auth test

--- src/pages/api/postToWebhook.ts
+++ src/pages/api/postToWebhook.ts
@@ -4,57 +4,56 @@ import { siteMetadata } from '../../data/siteMetadata'
 import { env } from '../../env.mjs'
 import { getServerAuthSession } from '../../server/auth'
 import { getThumbnailUrl } from '../../utils/youtube'
-import { isUserModeratorOrAbove } from './../../utils/auth'
 
 const postToWebhook = async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getServerAuthSession({ req, res })
   const play = JSON.parse(req.body as string) as Play & { user: { name: string | null } }
 
-  if (!session || !isUserModeratorOrAbove(session?.user.role ?? '')) {
-    res.status(401).json('Not authorised')
-  } else {
-    switch (req.method) {
-      case 'POST':
-        const params = {
-          username: siteMetadata.title,
-          avatar_url: '',
-          tts: false,
-          embeds: [
-            {
-              type: 'rich',
-              title: play.name,
-              description: play.description,
-              color: 0x00ffff,
-              timestamp: play.createdAt.toString(),
-              image: {
-                url: getThumbnailUrl(play.youtubeId),
-                height: 0,
-                width: 0,
-              },
-              author: {
-                name: play.user.name,
-                url: `http://hyhy.gg/user/${play.userId}`,
-              },
-              url: `http://hyhy.gg/play/${play.id}`,
+  // if (!session || !isUserModeratorOrAbove(session?.user.role ?? '')) {
+  //   res.status(401).json('Not authorised')
+  // } else {
+  switch (req.method) {
+    case 'POST':
+      const params = {
+        username: siteMetadata.title,
+        avatar_url: '',
+        tts: false,
+        embeds: [
+          {
+            type: 'rich',
+            title: play.name,
+            description: play.description,
+            color: 0x00ffff,
+            timestamp: play.createdAt.toString(),
+            image: {
+              url: getThumbnailUrl(play.youtubeId),
+              height: 0,
+              width: 0,
             },
-          ],
-        }
-        void fetch(env.WEBHOOK_URL, {
-          method: 'POST',
-          body: JSON.stringify(params),
-          headers: {
-            'Content-Type': 'application/json',
+            author: {
+              name: play.user.name,
+              url: `http://hyhy.gg/user/${play.userId}`,
+            },
+            url: `http://hyhy.gg/play/${play.id}`,
           },
-        })
+        ],
+      }
+      void fetch(env.WEBHOOK_URL, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
 
-        res.status(200).json({ success: true, message: `Play ${play.id} successfully posted` })
-        break
+      res.status(200).json({ success: true, message: `Play ${play.id} successfully posted` })
+      break
 
-      default:
-        res.setHeader('Allow', ['POST'])
-        res.status(405).json(`Method ${req.method ?? ''} not allowed`)
-    }
+    default:
+      res.setHeader('Allow', ['POST'])
+      res.status(405).json(`Method ${req.method ?? ''} not allowed`)
   }
+  // }
 }
 
 export default postToWebhook
