ba00ebcd8947c512fdd6163f241fff3412df8ef1
harryhartley <hhartley94@gmail.com>
2023-03-29 19:06:49 +0100

    add bookmarks to play page

--- src/server/api/routers/play.ts
+++ src/server/api/routers/play.ts
@@ -12,7 +12,10 @@ import {
 
 export const playRouter = createTRPCRouter({
   getById: publicProcedure.input(z.string().cuid()).query(({ ctx, input }) => {
-    return ctx.prisma.play.findUnique({ where: { id: input }, include: { user: { select: { name: true } } } })
+    return ctx.prisma.play.findUnique({
+      where: { id: input },
+      include: { user: { select: { name: true } }, bookmarks: { where: { userId: ctx.session?.user.id } } },
+    })
   }),
   getAllByUserId: publicProcedure
     .input(
