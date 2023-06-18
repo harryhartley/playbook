34ca9e111886c1fc515a3e74b0387a935916b34f
harryhartley <hhartley94@gmail.com>
2023-06-18 21:52:35 +0100

    wrong thumbnail id

--- src/utils/video.ts
+++ src/utils/video.ts
@@ -1,6 +1,6 @@
 export const getThumbnailUrl = (videoEmbedUrl: string): string => {
   if (videoEmbedUrl.includes('drive.google.com')) {
-    return `https://drive.google.com/thumbnail?id=${videoEmbedUrl.split('/')[6] ?? ''}`
+    return `https://drive.google.com/thumbnail?id=${videoEmbedUrl.split('/')[5] ?? ''}`
   }
   return `https://img.youtube.com/vi/${videoEmbedUrl.slice(30, 41)}/hqdefault.jpg`
 }
